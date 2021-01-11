105462a568691231eaad57fc816f9c802f280976
bkhang2976 <bkhang2976@gmail.com>
2021-01-11 13:07:25 +0900

    main method and method testing

--- src/doitAlgo/Median.java
+++ src/doitAlgo/Median.java
@@ -23,6 +23,20 @@ public class Median {
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Finding the median value between 3 integers");
         
+        System.out.println("Input a's value");
+        int a = input.nextInt();
+
+        System.out.println("Input b's value");
+        int b = input.nextInt();
+
+        System.out.println("Input c's value");
+        int c = input.nextInt();
+
+        System.out.println("The median is ="+ med3(a, b, c));
+
     }
 }
